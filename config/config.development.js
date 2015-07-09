c8740c585c05e58d54b9acfc1b5a5fa513c195c9
aaronw3373 <aaronw3373@gmail.com>
2015-07-08 23:00:44 -0400

    new db

--- config/config.development.js
+++ config/config.development.js
@@ -6,6 +6,7 @@ config.serverPort = 3000;
 
 config.mongo = {};
 config.mongo.dbUrl = 'mongodb://localhost/powplanner';
+// config.mongo.dbUrl = 'mongodb://localhost/dbclakre';
 // config.mongo.dbUrl = 'mongodb://aaronw3373:password1@ds045882.mongolab.com:45882/clarkedb';
 
 module.exports = config;
